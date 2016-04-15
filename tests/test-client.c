e3c9918cdb8865d21169f3be54abb84c524ea591
Patrick Griffis <tingping@tingping.se>
2016-04-15 19:30:19 -0400

    Start adding tests

---
+++ tests/test-client.c
@@ -0,0 +1,38 @@
+/* test-client.c
+ *
+ * Copyright (C) 2016 Patrick Griffis <tingping@tingping.se>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib.h>
+#include "signal-glib.h"
+
+static void
+test_init (void)
+{
+  signal_init ();
+  signal_deinit ();
+}
+
+int
+main (int    argc,
+	  char **argv)
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/signal/init", test_init);
+
+  return g_test_run ();
+}
